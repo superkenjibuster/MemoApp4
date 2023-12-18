cddb1e2bb25bd10fb6a04de01ac14a6c4de608e7
superkenjibuster <145850476+superkenjibuster@users.noreply.github.com>
2023-12-18 21:31:08 +0900

    lesson99 fix

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,21 +1,21 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, FlatList } from 'react-native'
 import { router, useNavigation } from 'expo-router'
-import { useEffect/* , useState */ } from 'react'
-import { collection, onSnapshot, query/* , orderBy  */ } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { collection, onSnapshot, query, orderBy } from 'firebase/firestore'
 
 import MemoListItem from '../../components/MemoListItem'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import LogOutButton from '../../components/LogOutButton'
 import { db, auth } from '../../config'
-/* import { type Memo } from '../../../types/memo' */
+import { type Memo } from '../../../types/memo'
 
 const handlePress = (): void => {
   router.push('/memo/create')
 }
 
 const List = (): JSX.Element => {
-/*   const [memos, setMemos] = useState([]) */
+  const [memos, setMemos] = useState<Memo[]>([])
   const navigation = useNavigation()
   useEffect(() => {
     navigation.setOptions({
@@ -25,35 +25,28 @@ const List = (): JSX.Element => {
   useEffect(() => {
     if (auth.currentUser === null) { return }
     const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
-    const q = query(ref/* , orderBy('updateAt', 'desc') */)
-    /* const unsubscribe = onSnapshot(q, (snapshot) => { */
-    const remoteMemos = []
-
-    onSnapshot(q, (snapshot) => {
+    const q = query(ref, orderBy('updateAt', 'desc'))
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      const remoteMemos: Memo[] = []
       snapshot.forEach((doc) => {
-        const data = doc.data()
-        console.log(doc.id, 'bodyText', data.bodyText.substr(0, 10), 'updateAt', data.updateAt)
-        /* console.log('memo', doc.id) */
-        /* const { bodyText, updateAt } = doc.data() */
+        console.log('memo', doc.data())
+        const { bodyText, updateAt } = doc.data()
         remoteMemos.push({
           id: doc.id,
-          bodyText: data.bodyText,
-          updateAt: data.updateAt
+          bodyText,
+          updateAt
         })
       })
-      console.log('remoteMemos', remoteMemos)
-      /*       setMemos(remoteMemos) */
+      setMemos(remoteMemos)
     })
-    /* return unsubscribe */
+    return unsubscribe
   }, [])
   return (
     <View style={styles.container}>
-
-        <View>
-         <MemoListItem />
-         <MemoListItem />
-         <MemoListItem />
-        </View>
+      <FlatList
+      data={memos}
+      renderItem={({ item }) => <MemoListItem memo={item}/> }
+      />
 
         <CircleButton onPress={handlePress}>
             <Icon name='plus' size={40} color='#ffffff'/>
--- src/components/MemoListItem.tsx
+++ src/components/MemoListItem.tsx
@@ -2,14 +2,23 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
 
 import Icon from './Icon'
+import { type Memo } from '../../types/memo'
 
-const MemoListItem = (): JSX.Element => {
+interface Props {
+  memo: Memo
+}
+
+const MemoListItem = (props: Props): JSX.Element | null => {
+  const { memo } = props
+  const { bodyText, updateAt } = memo
+  if (bodyText === null || updateAt === null) { return null }
+  const dateString = updateAt.toDate().toLocaleString('ja-JP')
   return (
     <Link href='/memo/detail' asChild>
       <TouchableOpacity style={styles.memoListItem}>
           <View>
-              <Text style={styles.memoListItemTitle}>買い物リスト</Text>
-              <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
+              <Text numberOfLines={1} style={styles.memoListItemTitle}>{bodyText}</Text>
+              <Text style={styles.memoListItemDate}>{dateString}</Text>
           </View>
           <TouchableOpacity>
               <Icon name='delete' size={32} color='#B0B0B0' />
